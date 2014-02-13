6b28ec846f3da1946f5ea89302d1f9e3fb1197cc
FabienNollet <fabien.nollet@telecomnancy.net>
2014-02-13 09:56:38 +0100

    added the two others kind of triplet
    still have to convert gene mim to gene id and find the good disease name

--- OMIMParser/src/omimparser/OMIMParser.java
+++ OMIMParser/src/omimparser/OMIMParser.java
@@ -11,6 +11,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.concurrent.ConcurrentSkipListSet;
 import org.apache.lucene.queryParser.ParseException;
 
 /**
@@ -22,8 +23,9 @@ public class OMIMParser {
     private SimpleLuceneSearch lucene;
     private FileInputStream in;
     private FileOutputStream out;
-    private int disease, diseaseCount;
+    private int diseaseColNbr, diseaseCount;
     private FileInputStream mim2gene;
+    private ConcurrentSkipListSet<String> genes, diseases;
 
     public static final int GENEMAP1 = 1;
     public static final int GENEMAP2 = 2;
@@ -32,14 +34,16 @@ public class OMIMParser {
         lucene = new SimpleLuceneSearch(index);
         this.in = new FileInputStream(in);
         this.out = new FileOutputStream(out);
+        genes = new ConcurrentSkipListSet<>(String.CASE_INSENSITIVE_ORDER);
+        diseases = new ConcurrentSkipListSet<>(String.CASE_INSENSITIVE_ORDER);
         //mim2gene = new FileInputStream("indexOnUmlsOmim");
         switch (type) {
             case GENEMAP1:
-                disease = 13;
+                diseaseColNbr = 13;
                 diseaseCount = 3;
                 break;
             case GENEMAP2:
-                disease = 11;
+                diseaseColNbr = 11;
                 diseaseCount = 1;
                 break;
 
@@ -47,7 +51,7 @@ public class OMIMParser {
     }
 
     /**
-     * @param args the command lreadere arguments
+     * @param args diseaseColNbrPropertyhe command lreadere argumendiseaseColNbrPropertys
      */
     public static void main(String[] args) {
         // TODO code application logic here
@@ -80,19 +84,29 @@ public class OMIMParser {
         BufferedReader reader = new BufferedReader(new InputStreamReader(this.in));
         OutputStreamWriter writer = new OutputStreamWriter(this.out);
         String line = reader.readLine();
-        Triplet t = new Triplet();
+        Triplet diseaseProperty = new Triplet();
+        Triplet hasLabel = new Triplet();
         writer.write("@prefix <di: http://telecomnancy.eu/disease/> .\r\n");
         writer.write("@prefix <ge: http://telecomnancy.eu/gene/> .\r\n");
         writer.write("@prefix <omim: http://telecomnancy.eu/omim/> .\r\n");
+        writer.write("@prefix <rdfs: http://www.w3c.org/2000/01rdf-schema#> .\r\n");
         String[] data;
         String tmp;
         int err = 0, total = 0;
+        hasLabel.setProperty("rdfs:hasLabel");
         while (line != null) {
-            t.setObject("ge:" + getField(line, 5).split(",")[0]);
-            t.setProperty("omim:involvedInMechanismOf");
+            tmp = getField(line, 5).split(",")[0];
+            hasLabel.setSubject(tmp);
+            //tmp = getGeneId(tmp);
+            hasLabel.setObject("ge:" + tmp);
+            diseaseProperty.setObject("ge:" + tmp);
+            diseaseProperty.setProperty("omim:involvedInMechanismOf");
+            if(genes.add(tmp)){
+                writer.write(hasLabel + "\r\n");
+            }
             tmp = "";
             for (int i = 0; i < diseaseCount; i++) {
-                tmp += getField(line, disease + i);
+                tmp += getField(line, diseaseColNbr + i);
             }
             data = tmp.split(";");
             String id;
@@ -102,8 +116,13 @@ public class OMIMParser {
                     total ++;
                     id = lucene.getCuidFromMimId(data1);
                     if (id != null && !id.equals("")) {
-                        t.setSubject("di:" + id);
-                        writer.write(t + "\r\n");
+                        diseaseProperty.setSubject("di:" + id);
+                        writer.write(diseaseProperty + "\r\n");
+                        if(diseases.add(id)){
+                            hasLabel.setObject("di:" + id);
+                            hasLabel.setSubject(data1);
+                            writer.write(hasLabel + "\r\n");
+                        }
                     }else{
                         err ++;
                     }
