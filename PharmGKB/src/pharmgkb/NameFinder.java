49a159340f53fb0803e6253e2e2e5240ea295c4f
vanessadegre <vanessa.degre@telecomnancy.net>
2014-02-13 09:01:49 +0100

    parseur PharmGKB

---
+++ PharmGKB/src/pharmgkb/NameFinder.java
@@ -0,0 +1,43 @@
+package pharmgkb;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class NameFinder {
+	
+	public HashMap<String, String> dictEntrez;
+	public HashMap<String, String> dictName;
+	
+	public NameFinder(String fileName){
+		dictEntrez = new HashMap<String, String>();
+		dictName = new HashMap<String, String>();
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(fileName));
+			String line;
+			String[] processedLine;
+			br.readLine();
+			while ((line = br.readLine()) != null) {
+				processedLine= line.split("\t");
+				dictEntrez.put(processedLine[0], processedLine[1]);	
+				dictName.put(processedLine[0], processedLine[3]);	
+		    }
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public HashMap<String, String> getDictEntrez() {
+		return dictEntrez;
+	}
+	
+	public HashMap<String, String> getDictName() {
+		return dictName;
+	}
+}
---
+++ PharmGKB/src/tncy/plbc/MeshIdToCui.java
@@ -0,0 +1,48 @@
+package tncy.plbc;
+
+/**
+ * @author: Adrien Coulet
+ * @date: June, 26, 2012
+ * Obtain the CUI of a drug from a drug name  
+ */
+
+
+import java.io.IOException;
+
+import org.apache.lucene.index.CorruptIndexException;
+import org.apache.lucene.queryParser.ParseException;
+
+import tncy.plbc.SimpleLuceneSearch;;
+
+/** 
+ * This class instantiate the class Simple Lucene Search to do a search on a lucene index
+ * It call the method getCuiFromMeshId that enables to obtain UMLS id form a MeSH id
+ * */
+public class MeshIdToCui {
+  
+  private MeshIdToCui() {}
+ 
+  public static void main(String[] args) {
+	 
+	  		String inputMeshId = "D003924";
+			
+			SimpleLuceneSearch searchInMrConso;
+			try {
+				searchInMrConso = new SimpleLuceneSearch("index/indexOnMesh2012");
+				
+				String cui=searchInMrConso.getCuiFromMeshId(inputMeshId);
+				
+				System.out.println("the MeSH id "+inputMeshId+" has for UMLS CUI : "+cui);
+				
+			} catch (CorruptIndexException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			
+  }
+  
+}
+
