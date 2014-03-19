99eff32ed792be8327d44e8d4ef1a90efd55ed88
FabienNollet <fabien.nollet@telecomnancy.net>
2014-03-19 11:33:41 +0100

    ...

--- OMIMParser/src/omimparser/OMIMParser.java
+++ OMIMParser/src/omimparser/OMIMParser.java
@@ -37,8 +37,8 @@ public class OMIMParser {
         genes = new ConcurrentSkipListSet<>(String.CASE_INSENSITIVE_ORDER);
         diseases = new ConcurrentSkipListSet<>(String.CASE_INSENSITIVE_ORDER);
         mim2gene = new BufferedReader(new InputStreamReader(new FileInputStream("mim2gene.txt")));
-		initGenes(mim2gene);
-		
+        initGenes(mim2gene);
+
         switch (type) {
             case GENEMAP1:
                 diseaseColNbr = 13;
@@ -53,14 +53,17 @@ public class OMIMParser {
     }
 
     /**
-     * @param args diseaseColNbrPropertyhe command lreadere argumendiseaseColNbrPropertys
+     * @param args diseaseColNbrPropertyhe command lreadere
+     * argumendiseaseColNbrPropertys
      */
     public static void main(String[] args) {
         // TODO code application logic here
         try {
             OMIMParser parser;
+            long tempsDebut = System.currentTimeMillis();
             parser = new OMIMParser(args[0], args[1], args[2], Integer.parseInt(args[3]));
             parser.toRDFFile();
+            System.out.println("temps de génération : " + (System.currentTimeMillis() - tempsDebut) + " ms");
         } catch (Exception e) {
             System.err.println(e);
         }
@@ -94,11 +97,16 @@ public class OMIMParser {
         writer.write("@prefix rdfs: <http://www.w3c.org/2000/01rdf-schema#> .\r\n");
         String[] data;
         String tmp;
-        int noId = 0, total = 0, unfoundId = 0;
+        int noId = 0, total = 0, unfoundId = 0, geneId = 0;
         hasLabel.setProperty("rdfs:hasLabel");
         while (line != null) {
             tmp = getField(line, 5).split(",")[0];
             tmp = GeneSymbolToID(tmp);
+            if (tmp == null || tmp.equals("")) {
+                geneId++;
+                line = reader.readLine();
+                continue;
+            }
             diseaseProperty.setObject("ge:" + tmp);
             diseaseProperty.setProperty("omim:involvedIn");
             tmp = "";
@@ -110,27 +118,28 @@ public class OMIMParser {
             for (String data1 : data) {
                 try {
                     data1 = data1.replaceAll("(.*), ?([0-9]+) ?\\([0-9]+\\)", "$1;$2");
-                    if(data1.split(";").length < 2){
-                        noId ++;
+                    if (data1.split(";").length < 2) {
+                        noId++;
                         continue;
                     }
                     id = data1.split(";")[1];
-                    for(int i = 1; i < id.length(); i++){
-                        if(id.charAt(i) < '0' || id.charAt(i) > '9')
+                    for (int i = 1; i < id.length(); i++) {
+                        if (id.charAt(i) < '0' || id.charAt(i) > '9') {
                             id = id.substring(0, i);
+                        }
                     }
                     id = lucene.getCuidFromMimId(id);
                     if (id != null && !id.equals("")) {
                         diseaseProperty.setSubject("di:" + id);
                         writer.write(diseaseProperty + "\r\n");
                         total++;
-                        if(diseases.add(id)){
+                        if (diseases.add(id)) {
                             hasLabel.setObject("di:" + id);
                             hasLabel.setSubject("\"" + data1.split(";")[0] + "\"");
                             writer.write(hasLabel + "\r\n");
                         }
-                    }else{
-                        unfoundId ++;
+                    } else {
+                        unfoundId++;
                     }
                 } catch (ParseException e) {
                     //err ++;
@@ -138,7 +147,7 @@ public class OMIMParser {
             }
             line = reader.readLine();
         }
-        for(String s : genes){
+        for (String s : genes) {
             data = s.split(" ");
             hasLabel.setObject("ge:" + data[1]);
             hasLabel.setSubject("\"" + data[0] + "\"");
@@ -147,39 +156,36 @@ public class OMIMParser {
         writer.close();
         reader.close();
         System.out.println("\n\n\n\n________________________________________________________");
-        System.out.println("deseases without IDs specified : " + noId);
+        System.out.println("diseases without IDs specified : " + noId);
         System.out.println("cuid from omime fails : " + unfoundId);
+        System.out.println("gene name to Id fails : " + geneId);
         System.out.println("involvedIn properties generated : " + total);
-        
+
     }
-	
 
-
-        
     private void initGenes(BufferedReader in) throws IOException {
-    	String line;
-    	String[] tmpLine;
-		
-			line = in.readLine();		
-		while(line != null){
-	    	tmpLine=line.split("	");
-	    	if (!tmpLine[3].equals("-")) {
-	    		genes.add(tmpLine[3]+" "+tmpLine[2]);
-	    	}
-	    	line = in.readLine();
-	    }
-		in.close();		
+        String line;
+        String[] tmpLine;
+
+        line = in.readLine();
+        while (line != null) {
+            tmpLine = line.split("	");
+            if (!tmpLine[3].equals("-")) {
+                genes.add(tmpLine[3] + " " + tmpLine[2]);
+            }
+            line = in.readLine();
+        }
+        in.close();
     }
-    
-    
-    public String GeneSymbolToID(String symbol) throws IOException{
-    	String gene = genes.tailSet(symbol).first();
-    	String[] tmp= gene.split(" ");
-    	if(symbol.equals(tmp[0])){
-    		return tmp[1];
-    	}
-    	return null;
-    	
+
+    public String GeneSymbolToID(String symbol) throws IOException {
+        String gene = genes.tailSet(symbol).first();
+        String[] tmp = gene.split(" ");
+        if (symbol.equals(tmp[0])) {
+            return tmp[1];
+        }
+        return null;
+
     }
-    
+
 }
